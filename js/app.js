6268d74c1fb19d7c453e8ae8dfdb383dd95b6901
anfelespitia <39103110+anfelespitia@users.noreply.github.com>
2018-07-31 13:26:37 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 ///Enemy class
-class Enemy {
-    constructor(x, y, speed) {
+var Enemy {
+    function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
     this.x = 0;
